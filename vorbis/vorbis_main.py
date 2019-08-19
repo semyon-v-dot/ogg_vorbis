3f7eeb4f1a3de4cd3dc456223180083110bfcf31
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-08-19 11:31:54 +0500

    Refactor CodebookDecoder. Refactor DataReader

--- vorbis/vorbis_main.py
+++ vorbis/vorbis_main.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Callable, Tuple
 
 from .ogg import PacketsReader, CorruptedFileDataError
-from .codebook import CodebookDecoder
-from .helper_funcs import ilog
+from .helper_funcs import *
 
 
 class EndOfPacketException(Exception):
@@ -11,100 +10,9 @@ class EndOfPacketException(Exception):
     pass
 
 
-class DataReader:
-    """Class for low-level data reading"""
-    _current_packet: bytes
-    byte_pointer: int = 0
-    bit_pointer: int = 0
-
-    def __init__(self, filename: Optional[str] = None, data: bytes = b''):
-        if filename is not None:
-            self._packets_reader = PacketsReader(filename)
-        self._current_packet = data
-
-    def close_file(self):
-        """Method closes opened ogg-vorbis file"""
-        self._packets_reader.close_file()
-
-    def restart_file_reading(self):
-        """Resets file and packet pointers to zero"""
-        self.set_global_position(0)
-        self._current_packet = b''
-        self.byte_pointer = 0
-        self.bit_pointer = 0
-
-    def set_global_position(self, new_position: int):
-        """Method moves global position of [byte_pointer] in audio file"""
-        self._packets_reader.move_byte_position(new_position)
-
-    def get_global_position(self):
-        """Method returns global position of [byte_pointer] in audio file"""
-        return self._packets_reader.opened_file.tell()
-
-    def read_packet(self):
-        """Method reads packet from [packets_reader]"""
-        self._current_packet = self._packets_reader.read_packet()[0]
-        self.byte_pointer = self.bit_pointer = 0
-
-    def read_bit(self) -> int:
-        """Method reads and return one bit from current packet data"""
-        try:
-            required_bit = bool(self._current_packet[self.byte_pointer]
-                                & (1 << self.bit_pointer))
-        except IndexError:
-            raise EndOfPacketException('End of packet condition triggered')
-
-        self.bit_pointer += 1
-        if self.bit_pointer == 8:
-            self.bit_pointer = 0
-            self.byte_pointer += 1
-
-        return int(required_bit)
-
-    def read_bits(self, bits_count: int) -> str:
-        """Method reads and return several bits from current packet data"""
-        assert bits_count >= 0
-
-        read_bits = ''
-        for i in range(bits_count):
-            read_bits = str(self.read_bit()) + read_bits
-
-        return read_bits
-
-    def read_byte(self) -> bytes:
-        """Method reads and return one byte from current packet"""
-        return bytes([self.read_bits_for_int(8)])
-
-    def read_bytes(self, bytes_count: int) -> bytes:
-        """Method reads and return several bytes from current packet"""
-        assert bytes_count >= 0
-
-        read_bytes = b''
-        for i in range(bytes_count):
-            read_bytes += self.read_byte()
-
-        return read_bytes
-
-    def read_bits_for_int(self, bits_count: int, signed: bool = False) -> int:
-        """Reads bits from current packet for int value
-
-        Reads [bits_count] bits from current packet and return unsigned int
-        value"""
-        assert bits_count >= 0
-
-        number = self.read_bits(bits_count)
-
-        if not signed or number[0] == '0':
-            return int(number, 2)
-        else:
-            number = int(number, 2) - 1
-            return -(number
-                     ^ int(''.join(['1'] * bits_count), 2))
-
-
 class PacketsProcessor:
     """Class for processing packets of vorbis bitstream"""
-    def __init__(self, filename):
+    def __init__(self, filename: str):
         self._data_reader = DataReader(filename)
 
         self._read_bit = self._data_reader.read_bit
@@ -660,3 +568,396 @@ objects"""
                     packet_type = self._read_byte()
         except EOFError:
             self._data_reader.restart_file_reading()
+
+
+class CodebookDecoder:
+    """Class represents decoder of vorbis codebooks"""
+    _read_bit: Callable[[], int]
+    _read_bits: Callable[[int], str]
+    _read_bits_for_int: Callable[[int, bool], int]
+
+    _read_byte: Callable[[], bytes]
+    _read_bytes: Callable[[int], bytes]
+
+    def __init__(self, data_reader):
+        data_reader: DataReader
+
+        self._read_bit = data_reader.read_bit
+        self._read_bits = data_reader.read_bits
+        self._read_bits_for_int = data_reader.read_bits_for_int
+
+        self._read_byte = data_reader.read_byte
+        self._read_bytes = data_reader.read_bytes
+
+    # Amount of scalars in every VQ table vector
+    _codebook_dimensions: int
+
+    # Amount of codewords
+    _codebook_entries: int
+
+    # Flags for codewords lengths unpacking
+    _ordered: bool
+    _sparse: bool
+
+    # Data for Huffman tree decoding
+    _codebook_codewords_lengths: List[int]
+
+    # Acquired from Huffman tree decoding
+    # List of decoded codewords
+    _codebook_codewords: List[str]
+
+    # Data below for VQ lookup table unpacking
+    # TODO: Explain ALL vars below. It is needed for understanding VQ
+    #  lookup table
+    _codebook_lookup_type: int
+    _codebook_minimum_value: float
+    _codebook_delta_value: float
+    _codebook_value_bits: int
+    _codebook_sequence_p: bool
+    _codebook_lookup_values: int
+    _codebook_multiplicands: List[int]
+
+    # Acquired from vq lookup table unpacking
+    _VQ_lookup_table: List[List[float]]
+
+    def read_codebook(self) -> Tuple[List[str], List[List[float]], int]:
+        """Method reads full codebook from packet data"""
+        self._check_codebook_sync_pattern()
+
+        self._codebook_dimensions = int.from_bytes(
+            self._read_bytes(2), byteorder='little')
+
+        self._codebook_entries = int.from_bytes(
+            self._read_bytes(3), byteorder='little')
+
+        if self._codebook_entries == 1:
+            raise CorruptedFileDataError('Single codebook entry was given')
+
+        self._ordered = bool(self._read_bit())
+        if not self._ordered:
+            self._sparse = bool(self._read_bit())
+
+        self._codebook_codewords_lengths = self._read_codeword_lengths()
+
+        self._codebook_codewords = self._huffman_decode()
+
+        self._codebook_lookup_type = self._read_bits_for_int(4)
+
+        if (self._codebook_lookup_type < 0
+                or self._codebook_lookup_type > 2):
+            raise CorruptedFileDataError(
+                'Not supported lookup type: '
+                + str(self._codebook_lookup_type))
+
+        if self._codebook_lookup_type != 0:
+
+            self._codebook_minimum_value = float32_unpack(
+                self._read_bits_for_int(32))
+
+            self._codebook_delta_value = float32_unpack(
+                self._read_bits_for_int(32))
+
+            self._codebook_value_bits = self._read_bits_for_int(4) + 1
+
+            self._codebook_sequence_p = bool(self._read_bit())
+
+            if self._codebook_lookup_type == 1:
+                self._codebook_lookup_values = lookup1_values(
+                        self._codebook_entries, self._codebook_dimensions)
+            else:
+                self._codebook_lookup_values = (
+                        self._codebook_entries * self._codebook_dimensions)
+
+            self._codebook_multiplicands = []
+            for i in range(self._codebook_lookup_values):
+                self._codebook_multiplicands.append(
+                    self._read_bits_for_int(self._codebook_value_bits))
+
+            self._VQ_lookup_table = self._vq_lookup_table_unpack()
+
+            return (
+                self._codebook_codewords,
+                self._VQ_lookup_table,
+                self._codebook_lookup_type)
+
+        return self._codebook_codewords, [], 0
+
+    def _check_codebook_sync_pattern(self):
+        """Method checks if there is a codebook sync pattern in packet data"""
+        if self._read_bytes(3) != b'\x42\x43\x56':
+            raise CorruptedFileDataError('Codebook sync pattern is absent')
+
+    # Optimize: assert codeword_length < 32 ?
+    def _read_codeword_lengths(self) -> List[int]:
+        """Method reads codewords lengths from packet data"""
+        result_codeword_lengths: List[int] = []
+        if not self._ordered:
+            for i in range(self._codebook_entries):
+                if self._sparse:
+                    flag: bool = bool(self._read_bit())
+
+                    if flag:
+                        result_codeword_lengths.append(
+                            self._read_bits_for_int(5) + 1)
+                    else:
+                        result_codeword_lengths.append(-1)
+                else:
+                    result_codeword_lengths.append(
+                        self._read_bits_for_int(5) + 1)
+        else:
+            current_entry: int = 0
+            current_length: int = self._read_bits_for_int(5) + 1
+
+            while current_entry < self._codebook_entries:
+                number = self._read_bits_for_int(
+                    ilog(self._codebook_entries - current_entry))
+
+                for i in range(current_entry, current_entry + number):
+                    result_codeword_lengths.append(current_length)
+
+                current_entry = number + current_entry
+                current_length += 1
+
+                if current_entry > self._codebook_entries:
+                    raise CorruptedFileDataError(
+                        "Incorrect codebook lengths coding")
+
+        return result_codeword_lengths
+
+    def _huffman_decode(self) -> List[str]:
+        """Decodes Huffman tree with int codewords representation method"""
+        result_codewords: List[str] = []
+
+        for start_entry in range(self._codebook_entries):
+            if self._codebook_codewords_lengths[start_entry] != -1:
+                break
+            result_codewords.append('')
+        else:
+            return result_codewords
+
+        result_codewords.append(
+            ''.zfill(self._codebook_codewords_lengths[start_entry]))
+
+        available: List[int] = [0] * 32
+        for i in range(1, self._codebook_codewords_lengths[start_entry] + 1):
+            available[i] = 1 << (32 - i)
+
+        for i in range(start_entry + 1, self._codebook_entries):
+            max_available_branch = self._codebook_codewords_lengths[i]
+
+            if max_available_branch == -1:
+                result_codewords.append('')
+                continue
+            while (max_available_branch > 0
+                    and available[max_available_branch] == 0):
+                max_available_branch -= 1
+            assert 0 < max_available_branch < 32
+
+            result = available[max_available_branch]
+            available[max_available_branch] = 0
+
+            codeword: str = bin(bit_reverse(result))[2:]
+            codeword = (
+                    ''.zfill(
+                        self._codebook_codewords_lengths[i] - len(codeword))
+                    + codeword)
+            result_codewords.append(codeword)
+
+            if max_available_branch != self._codebook_codewords_lengths[i]:
+                for new_branch in range(
+                        self._codebook_codewords_lengths[i],
+                        max_available_branch,
+                        -1):
+                    assert available[new_branch] == 0
+                    available[new_branch] = result + (1 << (32 - new_branch))
+
+    # TODO: Understand this method
+    def _vq_lookup_table_unpack(self) -> List[List[float]]:
+        """Method decode VQ lookup table from some values from packet data"""
+        result_vq_table: List[List[float]] = []
+
+        if self._codebook_lookup_type == 1:
+            for lookup_offset in range(self._codebook_entries):
+                last = 0
+                index_divisor = 1
+                value_vector: List[float] = []
+                for i in range(self._codebook_dimensions):
+                    multiplicand_offset = \
+                        (lookup_offset // index_divisor) \
+                        % self._codebook_lookup_values
+                    value_vector.append(
+                        self._codebook_multiplicands[multiplicand_offset]
+                        * self._codebook_delta_value
+                        + self._codebook_minimum_value
+                        + last)
+                    if self._codebook_sequence_p:
+                        last = value_vector[i]
+                    index_divisor *= self._codebook_lookup_values
+
+                result_vq_table.append(value_vector)
+        else:
+            for lookup_offset in range(self._codebook_entries):
+                last = 0
+                multiplicand_offset = lookup_offset * self._codebook_dimensions
+                value_vector: List[float] = []
+                for i in range(self._codebook_dimensions):
+                    value_vector.append(
+                        self._codebook_multiplicands[multiplicand_offset]
+                        * self._codebook_delta_value
+                        + self._codebook_minimum_value
+                        + last)
+                    if self._codebook_sequence_p:
+                        last = value_vector[i]
+                    multiplicand_offset += 1
+
+                result_vq_table.append(value_vector)
+
+        return result_vq_table
+
+    def _huffman_decode_bfc(self):  # Extremely slow code! Use for tests ONLY
+        """Decodes Huffman tree with brute force method"""
+        return_values: List[str] = []
+        for i in range(0, self._codebook_entries):
+            if self._codebook_codewords_lengths[i] == -1:
+                return_values.append('')
+                continue
+
+            bfc_value: str = (
+                ''.join(["1" for i in range(
+                    self._codebook_codewords_lengths[i])]))
+            for value in return_values:
+                if len(value) == len(bfc_value) and \
+                   int(value, 2) < int(bfc_value, 2):
+                    bfc_value = value
+            if '0' not in bfc_value:
+                bfc_value = ''.zfill(self._codebook_codewords_lengths[i])
+
+            while '0' in bfc_value:
+                for value in return_values:
+                    prefix_length = min(len(value), len(bfc_value))
+                    if prefix_length != 0 and \
+                       value[:prefix_length] == bfc_value[:prefix_length]:
+                        break
+                else:
+                    break
+
+                bfc_value = (
+                    bin(int(bfc_value, 2) + 1)[2:]
+                    .zfill(self._codebook_codewords_lengths[i]))
+
+            return_values.append(bfc_value)
+
+        self._huffman_bfc_fullness_check(return_values)
+
+        return return_values
+
+    @staticmethod
+    def _huffman_bfc_fullness_check(codewords: List[str]):  # Slow code
+        """Method checks if decoded Huffman tree is full"""
+        for codeword_1 in codewords:
+            if len(codeword_1) == 0:
+                continue
+
+            paired_node_is_present = False
+            for codeword_2 in codewords:
+                if len(codeword_2) == 0 or codeword_1 == codeword_2:
+                    continue
+
+                min_len = min(len(codeword_1), len(codeword_2))
+                if codeword_1[:min_len - 1] == codeword_2[:min_len - 1]:
+                    paired_node_is_present = True
+                    break
+            if not paired_node_is_present:
+                raise CorruptedFileDataError(
+                    'Huffman tree is underspecified')
+
+
+class DataReader:
+    """Class for low-level data reading"""
+    _current_packet: bytes
+    byte_pointer: int = 0
+    bit_pointer: int = 0
+
+    def __init__(self, filename: Optional[str] = None, data: bytes = b''):
+        if filename is not None:
+            self._packets_reader = PacketsReader(filename)
+        self._current_packet = data
+
+    def close_file(self):
+        """Method closes opened ogg-vorbis file"""
+        self._packets_reader.close_file()
+
+    def restart_file_reading(self):
+        """Resets file and packet pointers to zero"""
+        self.set_global_position(0)
+        self._current_packet = b''
+        self.byte_pointer = 0
+        self.bit_pointer = 0
+
+    def set_global_position(self, new_position: int):
+        """Method moves global position of [byte_pointer] in audio file"""
+        self._packets_reader.move_byte_position(new_position)
+
+    def get_global_position(self):
+        """Method returns global position of [byte_pointer] in audio file"""
+        return self._packets_reader.opened_file.tell()
+
+    def read_packet(self):
+        """Method reads packet from [packets_reader]"""
+        self._current_packet = self._packets_reader.read_packet()[0]
+        self.byte_pointer = self.bit_pointer = 0
+
+    def read_bit(self) -> int:
+        """Method reads and return one bit from current packet data"""
+        try:
+            required_bit = bool(self._current_packet[self.byte_pointer]
+                                & (1 << self.bit_pointer))
+        except IndexError:
+            raise EndOfPacketException('End of packet condition triggered')
+
+        self.bit_pointer += 1
+        if self.bit_pointer == 8:
+            self.bit_pointer = 0
+            self.byte_pointer += 1
+
+        return int(required_bit)
+
+    def read_bits(self, bits_count: int) -> str:
+        """Method reads and return several bits from current packet data"""
+        assert bits_count >= 0
+
+        read_bits = ''
+        for i in range(bits_count):
+            read_bits = str(self.read_bit()) + read_bits
+
+        return read_bits
+
+    def read_byte(self) -> bytes:
+        """Method reads and return one byte from current packet"""
+        return bytes([self.read_bits_for_int(8)])
+
+    def read_bytes(self, bytes_count: int) -> bytes:
+        """Method reads and return several bytes from current packet"""
+        assert bytes_count >= 0
+
+        read_bytes = b''
+        for i in range(bytes_count):
+            read_bytes += self.read_byte()
+
+        return read_bytes
+
+    def read_bits_for_int(self, bits_count: int, signed: bool = False) -> int:
+        """Reads bits from current packet for int value
+
+        Reads [bits_count] bits from current packet and return unsigned int
+        value"""
+        assert bits_count >= 0
+
+        number = self.read_bits(bits_count)
+
+        if not signed or number[0] == '0':
+            return int(number, 2)
+        else:
+            number = int(number, 2) - 1
+            return -(number
+                     ^ int(''.join(['1'] * bits_count), 2))
