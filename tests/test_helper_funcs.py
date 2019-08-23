3bf56b2663af27034d77fcd908c535fb4f0f8824
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-08-23 13:04:49 +0500

    Refactor helper functions tests

--- tests/test_helper_funcs.py
+++ tests/test_helper_funcs.py
@@ -7,24 +7,38 @@ from.test_decoders import hex_str_to_bin_str
 
 # noinspection PyMethodMayBeStatic
 class HelperFunctionsTests(TestCase):
-    def test_ilog(self):
+    def test_ilog_zero(self):
         self.assertEqual(ilog(0), 0)
+
+    def test_ilog_positive_number(self):
         self.assertEqual(ilog(1), 1)
-        self.assertEqual(ilog(-1111), 0)
         self.assertEqual(ilog(7), 3)
+
+    def test_ilog_negative_number(self):
+        self.assertEqual(ilog(-1111), 0)
+        self.assertEqual(ilog(-112312), 0)
 
     def test_lookup1_values(self):
         self.assertEqual(lookup1_values(50, 3), 3)
         self.assertEqual(lookup1_values(1, 3), 1)
 
-    def test_float32_unpack(self):
-        assert float32_unpack(0x60A03250) == (12880 * pow(2, -15))
-        assert float32_unpack(0xE0A03250) == -(12880 * pow(2, -15))
+    def test_float32_unpack_positive_number(self):
+        self.assertEqual(float32_unpack(0x60A03250), 12880 * pow(2, -15))
+
+    def test_float32_unpack_negative_number(self):
+        self.assertEqual(float32_unpack(0xE0A03250), -12880 * pow(2, -15))
+        self.assertEqual(
+            float32_unpack(0b11100000000100000000000000000000), -1.0)
 
     def test_bit_reverse(self):
-        assert bit_reverse(1879048192) == 14
-        assert bit_reverse(64424509440) == 0
-        assert bit_reverse(48) == 201326592
+        self.assertEqual(
+            bit_reverse(0b1110000000000000000000000000000), 14)
+        self.assertEqual(
+            bit_reverse(0b110000), 0b00001100000000000000000000000000)
+
+    def test_bit_reverse_greater_than_2_exponent_32(self):
+        self.assertEqual(
+            bit_reverse(0b111100000000000000000000000000000000), 0)
 
     def test_hex_to_bin(self):
         self.assertEqual(
