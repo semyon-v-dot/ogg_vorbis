74d6ac5dbae4080661593e5eda3884864b939f25
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-08-30 12:08:54 +0500

    Remove __init__.py with python version check

--- vorbis/__init__.py
+++
@@ -1,6 +0,0 @@
-from sys import version_info as sys_version_info, exit as sys_exit
-
-
-if (sys_version_info.major < 3
-        or (sys_version_info.major == 3 and sys_version_info.minor < 7)):
-    sys_exit('Python version 3.7 or upper is required')
