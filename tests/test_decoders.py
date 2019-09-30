9f4b8f5ca9051bad2e87066aecb9e6195e14d097
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-09-30 17:28:02 +0500

    Add test for modes decoding

--- tests/test_decoders.py
+++ tests/test_decoders.py
@@ -4,7 +4,7 @@ from os.path import (
     join as os_path_join,
     dirname as os_path_dirname,
     abspath as os_path_abspath)
-from typing import List
+from typing import List, Tuple
 from sys import path as sys_path
 
 sys_path.append(os_path_join(
@@ -678,25 +678,6 @@ class SetupHeaderDecodingTests(TestCase):
         # 95 00 00 40 | 1001_0101 0000_0000 0000_0000 0100_0000
         # 00 01 00 00 | 0000_0000 0000_0001 0000_0000 0000_0000
         # 00 00 10 40 | 0000_0000 0000_0000 0001_0000 0100_0000
-        # 00 02 02 02 | 0000_0000 0000_0010 0000_0010 0000_0010
-        # 00 00 00 00 | 0000_0000 0000_0000 0000_0000 0000_0000
-        # 00 01 00 00 | 0000_0000 0000_0001 0000_0000 0000_0000
-        # 00 02 02 4F | 0000_0000 0000_0010 0000_0010 0100_1111
-        # 67 67 53 00 | 0110_0111 0110_0111 0101_0011 0000_0000
-        # 00 40 08 00 | 0000_0000 0100_0000 0000_1000 0000_0000
-        # 00 00 00 00 | 0000_0000 0000_0000 0000_0000 0000_0000
-        # 00 01 00 00 | 0000_0000 0000_0001 0000_0000 0000_0000
-        # 00 04 00 00 | 0000_0000 0000_0100 0000_0000 0000_0000
-        # 00 E1 6B FA | 0000_0000 1110_0001 0110_1011 1111_1010
-        # F1 13 BF D2 | 1111_0001 0001_0011 1011_1111 1101_0010
-        # D3 CC C7 FF | 1101_0011 1100_1100 1100_0111 1111_1111
-        # FF FF FF FF | 1111_1111 1111_1111 1111_1111 1111_1111
-        # 12 FF FF FF | 0001_0010 1111_1111 1111_1111 1111_1111
-        # FF AF FF FF | 1111_1111 1010_1111 1111_1111 1111_1111
-        # FF EC D1 12 | 1111_1111 1110_1100 1101_0001 0001_0010
-        # 91 97 2B B5 | 1001_0001 1001_0111 0010_1011 1011_0101
-        # 73 C3 65 90 | 0111_0011 1100_0011 0110_0101 1001_0000
-        # D6 88 7E B9 | 1101_0110 1000_1000 0111_1110 1011_1001
         #
         # (1) [001_0101]-> previous things
         # (000) [0_0000|1]-> vorbis_mapping_count
@@ -731,7 +712,60 @@ class SetupHeaderDecodingTests(TestCase):
     def test_mapping_decoding_first_flag_set(self):
         pass
 
-    # TODO: def test_modes_decoding(self):
+    def test_modes_decoding(self):
+        data_reader = DataReader(PATH_TEST_1)
+        setup_header_decoder: SetupHeaderDecoder = (
+            SetupHeaderDecoder(data_reader))
+
+        data_reader.read_packet()
+        data_reader.read_packet()
+        data_reader.read_packet()
+
+        data_reader.byte_pointer = 8
+
+        codebooks_configs: List[SetupHeaderDecoder.CodebookData] = []
+
+        for i in range(44):
+            codebooks_configs.append(setup_header_decoder.read_codebook())
+
+        for i in range(data_reader.read_bits_for_int(6) + 1):
+            placeholder = data_reader.read_bits_for_int(16)
+
+            self.assertEqual(placeholder, 0)
+
+        setup_header_decoder.read_floors(44)
+
+        setup_header_decoder.read_residues(codebooks_configs)
+
+        # 'test_1.ogg': amount of mappings = 2
+        setup_header_decoder.read_mappings(2, 2, 2)
+
+        # (122647, 1)
+        #
+        # 02          | 0000_0010                              
+        # 00 00 00 00 | 0000_0000 0000_0000 0000_0000 0000_0000
+        # 00 01 00 00 | 0000_0000 0000_0001 0000_0000 0000_0000
+        # 00 02 02 4F | 0000_0000 0000_0010 0000_0010 0100_1111
+        #
+        # [0]-> blockflag [000_001]-> vorbis_mode_count [0]-> previous things
+        # (0000_0000)
+        # [0000_0000|0000_0000]
+        # (0000_0000)
+        # [0000_0000|0000_0000]
+        # [0000_0000]-> vorbis_mode_mapping
+        # (0000_000) [1]
+        # (0000_0000|0000_000)
+        # (0000_000) [0|0000_0000||0000_000]
+        # (0000_0000|0000_000)
+        # (0000_001) [0|0000_0000|0000_000]
+        # 0000_001 [0|0000_001]-> vorbis_mode_mapping
+
+        modes_configs: List[Tuple[bool, int]] = (
+            setup_header_decoder.read_modes(2))
+
+        self.assertEqual(
+            [(False, 0), (True, 1)],
+            modes_configs)
 
 
 class HuffmanTests(TestCase):
